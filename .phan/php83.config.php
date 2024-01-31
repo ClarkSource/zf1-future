8d2e0c016680f78d3de960e84baf24bafa6e566c
matthias.sabl <matthias.sabl@finanzen.de>
2024-01-31 11:01:20 +0100

    DEV-72091 add compatibility check tools

---
+++ .phan/php83.config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'target_php_version' => '8.3',
+    'minimum_severity' => \Phan\Issue::SEVERITY_CRITICAL,
+    'suppress_issue_types' => [],
+    'directory_list' => ['library', 'test', 'vendor'],
+    'exclude_analysis_directory_list' => ['vendor/'],
+];
